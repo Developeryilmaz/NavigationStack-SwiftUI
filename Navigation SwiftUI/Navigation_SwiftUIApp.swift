cde9044fbef2e9936fe7f29a237c300a919d4ac0
YILMAZ ER <developer.yilmazer@gmail.com>
2023-05-20 06:49:07 +0300

    Initial Commit

---
+++ Navigation SwiftUI/Navigation_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_SwiftUIApp.swift
+//  Navigation SwiftUI
+//
+//  Created by YILMAZ ER on 20.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
